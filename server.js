7c5590fed138ca7fb457664db1b22078cd34cf6e
zulfiqar108@gmail.com <zulfiqar108@gmail.com>
2020-09-29 15:37:57 +0530

    Add home page

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.send('Hello from server')
+    
+})
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
