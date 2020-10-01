813989491539a9fcb55c3989422b76fcc2091a06
zulfiqar108@gmail.com <zulfiqar108@gmail.com>
2020-10-01 11:37:59 +0530

    added mongo client

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const port = process.env.PORT || 3000
 // app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello from server')
+    res.send('<h1>Hello from server</h>')
 
 })
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://liveschool110:liveschool110@cluster0.ou5lp.mongodb.net/sample_analytics?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("sample_analytics").collection("accounts");
+  // perform actions on the collection object
+  console.log(collection);
+  client.close();
+});
